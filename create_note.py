a47d389db0d2bb3ca05382997f405e090e2d0e89
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2024-01-05 17:30:35 +0800

    FIX: add link at the end of README if topics doesn't exist

--- create_note.py
+++ create_note.py
@@ -19,20 +19,25 @@ problem_number = title.split(".")[0]
 with open(f"{problem_number}.md", 'w', encoding='utf-8') as output_file:
     output_file.write(note_markdown)
 
+# link markdown in README
+link_text = title
+link_destination = f"{problem_number}.md"
+link = f"[{link_text}]({link_destination})"
+
 # find topic block
 topic = args.topic
 original_readme_content = None
 with open("README.md", 'r', encoding='utf-8') as readme_file:
     original_readme_content = readme_file.read()
 topic_index = original_readme_content.find(topic)
-next_topic_index = original_readme_content.find("##", topic_index)
 
-# add link in README
-link_text = title
-link_destination = f"{problem_number}.md"
-link = f"[{link_text}]({link_destination})"
+if topic_index == -1:
+    new_topic = f"## {topic}"
+    new_readme_content = f"{original_readme_content}\n{new_topic}\n\n{link}\n"
+else:
+    next_topic_index = original_readme_content.find("##", topic_index)
+    new_readme_content = f"{original_readme_content[:next_topic_index]}{link}\n\n{original_readme_content[next_topic_index:]}"
 
-new_readme_content = f"{original_readme_content[:next_topic_index]}{link}\n\n{original_readme_content[next_topic_index:]}"
-
+# write link into README
 with open("README.md", 'w', encoding='utf-8') as readme_file:
-    readme_file.write(f"{new_readme_content}")
+        readme_file.write(f"{new_readme_content}")
